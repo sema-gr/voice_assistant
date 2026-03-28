fd1db43fe4ddb56164f575b1b9a97f71bf2c7bee
arman455 <snositel99@gmail.com>
2026-03-28 20:22:12 +0200

    complited add_commands

--- voiceAssistant/core/management/commands/run_assistant.py
+++ voiceAssistant/core/management/commands/run_assistant.py
@@ -5,10 +5,12 @@ import threading
 from django.core.management.base import BaseCommand
 from core.models import AppCommand, VoiceResponse 
 from utils.finder import find_app_path 
-from utils.voice_engine import speak_async 
+from utils.voice_engine import speak_async
 import speech_recognition as sr
 
-APP_CASH = {}
+from core.management.commands.add_commands import add_new_app_command_voice
+
+APP_CACHE = {}
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
@@ -37,6 +39,10 @@ class Command(BaseCommand):
     def process_command(self, text, source):
         text = text.lower().strip()
 
+        if "додати команду" in text:
+            add_new_app_command_voice()
+            return
+
         if "відкрий" not in text:
             for resp in VoiceResponse.objects.all():
                 if resp.keyword and resp.keyword.lower() in text:
@@ -55,9 +61,9 @@ class Command(BaseCommand):
             speak_async("Я не знайшов таку команду")
             return
         
-        if found_app.app_name in APP_CASH and os.path.exists(APP_CASH[found_app.app_name]):
+        if found_app.app_name in APP_CACHE and os.path.exists(APP_CACHE[found_app.app_name]):
             speak_async(f'Відкриваю {found_app.app_name}')
-            self.launch_app(APP_CASH[found_app.app_name])
+            self.launch_app(APP_CACHE[found_app.app_name])
             return
         
         speak_async(f"Шукаю {found_app.app_name}")
@@ -65,7 +71,7 @@ class Command(BaseCommand):
         def find_and_launch():
             found_path = find_app_path(found_app.app_name)
             if found_path:
-                APP_CASH[found_app.app_name] = found_path
+                APP_CACHE[found_app.app_name] = found_path
                 found_app.path = found_path
                 found_app.save()
                 self.launch_app(found_path)
